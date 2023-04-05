29485832db2b4095118b372a93c967f7cbabf19d
Rolguard <76728853+Rolguard@users.noreply.github.com>
2023-04-05 11:34:02 +1000

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./components/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage.js";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
