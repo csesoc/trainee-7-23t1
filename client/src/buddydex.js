d3f5706116fa641bbd5d2a1530b1080b1ed4be56
eningfu <122142631+eningfu@users.noreply.github.com>
2023-06-02 12:42:24 +1000

    Added JS component to the buddydex.

--- client/src/buddydex.js
+++ client/src/buddydex.js
@@ -1,11 +1,40 @@
+function createPetElement(imageSrc, name, type, description) {
+    const petDiv = document.createElement('div');
+    petDiv.classList.add('pet');
+  
+    const petImage = document.createElement('img');
+    petImage.src = imageSrc;
+    petImage.width = '50px';
+    petDiv.appendChild(petImage);
+  
+    const petName = document.createElement('h3');
+    petName.textContent = name;
+    petDiv.appendChild(petName);
+  
+    const petType = document.createElement('p');
+    petType.textContent = type;
+    petDiv.appendChild(petType);
+  
+    const petDescription = document.createElement('p');
+    petDescription.textContent = description;
+    petDiv.appendChild(petDescription);
+  
+    return petDiv;
+  }
+  
+  // Usage example:
+  const petContainer1 = document.getElementById('pet1');
+  const dogElement = createPetElement('dog/dog-0.png', 'Milo', 'Dog', 'Just a cute dog.');
+  petContainer1.appendChild(dogElement);
+  
+  const petContainer2 = document.getElementById('pet2');
+  const catElement = createPetElement('cat/cat-0.png', 'Mittens', 'Cat', 'Just a cute cat.');
+  petContainer2.appendChild(catElement);
+  
+  const petContainer3 = document.getElementById('pet3');
+  const birdElement = createPetElement('bird/bird-0.png', 'Elon', 'Bird', 'Twitter bird.');
+  petContainer3.appendChild(birdElement);
 
-const Pet = ({ name}) => {
-    return (
-        <div className='btn'>
-            {text}
-    
-        </div>
-    )
-}
-
-export default Pet
+  const petContainer4 = document.getElementById('pet4');
+  const salinaElement = createPetElement('salina.png', 'Salina', 'Human', 'The better training lead.');
+  petContainer4.appendChild(salinaElement);
--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -3,8 +3,8 @@ import "../landing.css";
 const LandingPage = () => {
   return (
     <div class="landing_cont">
-      <div class="wrapper">
-        <h1>SLEEP BUDDIES</h1>
+      <div class="wrapper_lp">
+        <h1 class="landing_h">SLEEP BUDDIES</h1>
         <div class="start">
           <button class="button1">
             <a href="farm">
