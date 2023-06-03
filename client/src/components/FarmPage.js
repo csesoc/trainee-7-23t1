7fb60c11dba3d8a6354e08727fcb4c17bc49f64d
olivia~~ <olivia2005ding@gmail.com>
2023-06-03 15:26:14 +1000

    made layout for farm page

--- client/src/components/FarmPage.js
+++ client/src/components/FarmPage.js
@@ -1,4 +1,16 @@
-const FarmPage = () => {
-  return <div>Welcome to the farm page!</div>;
+import React from 'react';
+import Pet from './Pet';
+import Clock from './Clock';
+
+
+function FarmPage(props){
+  return (
+    <div className="Farm">
+      <h1>very doomed farm page</h1>
+      <p>too doomed</p>
+      <Clock />
+      <Pet tag="running" name="Salina" limbs="4" birthday="today"/>
+    </div>
+  );
 };
 export default FarmPage;
--- client/src/components/GraveyardPage.js
+++ client/src/components/GraveyardPage.js
@@ -1,4 +1,11 @@
+import Grave from './Grave'
+
 const GraveyardPage = () => {
-  return <div>Welcome to the graveyard page!</div>;
+  return( 
+    <div id="Graveyard">Welcome to the graveyard page!
+      <Grave petImage="https://media.discordapp.net/attachments/1082563227851956234/1100655896239018064/salina.png?width=850&height=1134"
+      name="salina" deathDate="today" funFact="Sleep is good for you." />
+    </div>
+  )
 };
 export default GraveyardPage;
