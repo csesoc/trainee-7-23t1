d7b42b0186990e8d12391d76efd6c0747e06425b
olivia~~ <olivia2005ding@gmail.com>
2023-06-04 02:04:57 +1000

    redid the 'summon pets' to only summon uncollected pets and implemented a murder pet function

--- client/src/components/FarmPage/FarmPage.js
+++ client/src/components/FarmPage/FarmPage.js
@@ -7,35 +7,69 @@ import { useState } from "react";
 function FarmPage({ pets, updateCollectedStatus, updateLimbStatus }) {
   const [petCount, setCount] = useState(1);
 
+  // const handleCollectClick = (petName) => {
+  //   // Find the index of the pet being clicked
+  //   const clickedPetIndex = pets.findIndex((pet) => pet.name === petName);
+
+  //   // Generate a random index excluding the clicked pet
+  //   const randomIndex = Math.floor(Math.random() * pets.length);
+  //   const randomPetIndex =
+  //     randomIndex === clickedPetIndex
+  //       ? (randomIndex + 1) % pets.length
+  //       : randomIndex;
+
+  //   // Get the name of the randomly selected pet
+  //   const randomPetName = pets[randomPetIndex].name;
+
+  //   // Call the updateCollectedStatus function to update the collected status of both pets
+  //   setCount(petCount + 1);
+  //   updateCollectedStatus(petName, true);
+  //   updateCollectedStatus(randomPetName, true);
+  // };
+
   const handleCollectClick = (petName) => {
-    // Find the index of the pet being clicked
-    const clickedPetIndex = pets.findIndex((pet) => pet.name === petName);
-
-    // Generate a random index excluding the clicked pet
-    const randomIndex = Math.floor(Math.random() * pets.length);
-    const randomPetIndex =
-      randomIndex === clickedPetIndex
-        ? (randomIndex + 1) % pets.length
-        : randomIndex;
-
+    // Filter the uncollected pets
+    const uncollectedPets = pets.filter((pet) => !pet.collected);
+  
+    if (uncollectedPets.length === 0) {
+      // Handle the case when all pets are already collected
+      console.log("All pets are already collected.");
+      return;
+    }
+    // Generate a random index from the uncollectedPets array
+    const randomIndex = Math.floor(Math.random() * uncollectedPets.length);
+  
     // Get the name of the randomly selected pet
-    const randomPetName = pets[randomPetIndex].name;
-
+    const randomPetName = uncollectedPets[randomIndex].name;
+  
     // Call the updateCollectedStatus function to update the collected status of both pets
     setCount(petCount + 1);
-    updateCollectedStatus(petName, true);
     updateCollectedStatus(randomPetName, true);
   };
-
+  
   const collectedPets = pets.filter((pet) => pet.collected);
-
+  
   // Testing out the elimination options
   const handleMurderClick = (petName) => {
+    // Filters the collected pets
+    const collectedPets = pets.filter((pet) => pet.collected);
+    
+    if (collectedPets.length === 0) {
+      // Write some death message idk
+      return;
+    }
+    
     const randomIndex = Math.floor(Math.random() * collectedPets.length);
-    const randomPetIndex = 
-      randomIndex === clickedPetIndex
-      ? (randomIndex + 1) % pets.length
-      : randomIndex;
+    
+    const randomPetName = collectedPets[randomIndex].name;
+    const randomPetLimbs = collectedPets[randomIndex].limbs - 1; 
+    
+    if(randomPetLimbs === 0) {
+      updateLimbStatus(randomPetName, 4);
+      updateCollectedStatus(randomPetName, false);
+      return;
+    }
+    updateLimbStatus(randomPetName, randomPetLimbs);
   }
   
   return (
@@ -75,7 +109,13 @@ function FarmPage({ pets, updateCollectedStatus, updateLimbStatus }) {
         >
           Summon Pet
         </button>
-        <button>Night mode</button>
+        <button
+          onClick={() => {
+            console.log('OUCH...')
+            handleMurderClick(pets);
+          }}
+        >Night mode
+        </button>
       </div>
     </div>
   );
